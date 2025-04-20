74375a83844a72ba68d4bae164488cf2a9749582
azharfarhann <azharfarhan785@gmail.com>
2025-04-20 14:12:42 +0000

    update gitCli post method Done

--- gitCli.js
+++ gitCli.js
@@ -12,12 +12,20 @@ app.use(express.json());
 app.post("/git",async(req,res) => {
     try {
         // let userName = readline.question("enter github username:  ");
-        let userName= req.body
-        if(!userName) {
-            console.log("please enter valid username:  ");
-        }
-        let response = await axios.get("https://api.github.com/users/azharfarhann")
+        let userName  = req.body.userName
+        let response = await axios.get(`https://api.github.com/users/${userName}`);
         console.log(response.data);
+        let name = response.data.name;
+        let bio = response.data.bio;
+        let location = response.data.location;
+        let followers = response.data.followers;
+        let following = response.data.following;
+        
+        res.status(200).send(`data of ${userName} :
+            name        :  ${name}
+            location    :  ${location}
+            followers   :  ${followers}
+            following   :  ${following}`);
     } catch (error) {
         console.log(error);
         res.status(400).json({msg: error});
